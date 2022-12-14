ff458c043df0a5b4bf6ab2ed433a811e3275b2c4
Fanel Adrian <faneladrianpop@gmail.com>
2022-12-14 16:50:36 +0100

    historicoChat

--- src/app/components/tarjeta-chat/tarjeta-chat.component.ts
+++ src/app/components/tarjeta-chat/tarjeta-chat.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-tarjeta-chat',
+  selector: 'app-tarjeta-chats',
   templateUrl: './tarjeta-chat.component.html',
   styleUrls: ['./tarjeta-chat.component.css']
 })
