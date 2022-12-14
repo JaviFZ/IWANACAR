a1922fb7fa650c5777c69298282bb10871398136
PabloSarsotoMa <p.sarsoto.m@gmail.com>
2022-12-14 12:25:32 +0100

    cambio de rama

---
+++ src/app/models/tarjeta-viaje.spec.ts
@@ -0,0 +1,7 @@
+import { TarjetaViaje } from './tarjeta-viaje';
+
+describe('TarjetaViaje', () => {
+  it('should create an instance', () => {
+    expect(new TarjetaViaje()).toBeTruthy();
+  });
+});
