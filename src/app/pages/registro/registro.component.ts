e9da9c4efb53193b290461b54777c3a9dde42f08
JaviFZ <jferzap@gmail.com>
2022-12-15 12:53:52 +0100

    registro in progress

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -7,5 +7,22 @@ import {Router} from '@angular/router';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
-  
+  public nuevoUsuario : string[];
+
+    constructor(private router : Router){
+
+      this.nuevoUsuario = [];
+    }
+    public registrar(nombre:string, apellidos:string, fechaNac:string, email:string, password:string, password2:string){
+      if(password === password2){
+        this.nuevoUsuario.push(nombre, apellidos, fechaNac, email, password);
+        this.router.navigateByUrl('/login');
+        console.log(this.nuevoUsuario);
+        
+      }
+      else{
+        console.log("Las contraseñas no coinciden");
+        
+      }
+    }
 }
