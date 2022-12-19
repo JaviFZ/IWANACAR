2ad37361c93ed8c310d49405f62f053d64a381a3
CarlosGorostiaga <carlosgorospo@gmail.com>
2022-12-19 11:07:58 +0100

    subida de modificacion del añadir coche

--- src/app/pages/anadir-coche/anadir-coche.component.ts
+++ src/app/pages/anadir-coche/anadir-coche.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-anadir-coche',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./anadir-coche.component.css']
 })
 export class AnadirCocheComponent {
+
+  public marca : string;
+  public modelo : string;
+  public matricula : string;
+  public color : string;
+  public combustible : string;
+  public fumar : boolean;
+  public mascotas : boolean;
+  public desvios : boolean;
+  public tiempo : string;
+
+  constructor(private router : Router) { 
+    this.marca = "Seat";
+    this.modelo = "Ibiza";
+    this.matricula = "1234ABC";
+    this.color = "Rojo";
+    this.combustible = "Gasolina";
+    this.fumar = true;
+    this.mascotas = true;
+    this.desvios = true;
+    this.tiempo = "5min";
+
+}
+public ValidateCar (marca, modelo, matricula, color, combustible, fumar, mascotas, desvios, tiempo){
+  if(marca == this.marca && modelo == this.modelo && matricula == this.matricula && color == this.color && combustible == this.combustible && fumar == this.fumar && mascotas == this.mascotas && desvios == this.desvios && tiempo == this.tiempo){
+    this.router.navigateByUrl('/misCoches');
+  }
+  else{
+    console.log("Todos los Datos deben ser rellenados");
+  }
+}
 
 }
