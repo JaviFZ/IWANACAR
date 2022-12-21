dac03157c5c61ffe2238fa47056e902506b2c86a
PabloSarsotoMa <p.sarsoto.m@gmail.com>
2022-12-21 10:15:44 +0100

    eliminacion de paradas y servicio usuario

--- src/shared/usuario.service.ts
+++ src/shared/usuario.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/usuario';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
-
-  constructor(private router:Router) { 
-
+  public url:string;
+  public logueado:boolean;
+  public usuario:Usuario;
+  constructor(private http:HttpClient) { 
+    this.url = "http://localhost:3000"
+    this.logueado = false;
   }
+
 }
