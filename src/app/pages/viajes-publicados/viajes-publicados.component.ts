eb1c4687f7b9d5ad3dddd722783fffe44c7b1de3
Fanel Adrian <faneladrianpop@gmail.com>
2023-01-12 17:15:07 +0100

    responsive

--- src/app/pages/viajes-publicados/viajes-publicados.component.ts
+++ src/app/pages/viajes-publicados/viajes-publicados.component.ts
@@ -10,20 +10,20 @@ import { ViajeService } from 'src/shared/viaje.service';
   styleUrls: ['./viajes-publicados.component.css']
 })
 export class ViajesPublicadosComponent implements OnInit{
-  public viajes_publicados: [];
-  public pasajeros: [];
+  public viajes_publicados: any[];
+  public pasajeros: any[];
   constructor(private httpClient: HttpClient, private usuarioService: UsuarioService, public viajeService: ViajeService){
   
   }
   public ngOnInit(): void {
-    this.viajeService.viajesPublicados(this.usuarioService.usuario.id_usuario).subscribe((data:[])=>{
+    this.viajeService.viajesPublicados(this.usuarioService.usuario.id_usuario).subscribe((data:any[])=>{
       console.log(data);
       this.viajes_publicados = data
     })
     this.anadirPasajeros();
   }
   public anadirPasajeros(){
-    this.viajeService.pasajeros(this.usuarioService.usuario.id_usuario).subscribe((result:[])=>{
+    this.viajeService.pasajeros(this.usuarioService.usuario.id_usuario).subscribe((result:any[])=>{
       console.log(result);
       if(result != undefined){
         this.pasajeros = result
@@ -32,5 +32,11 @@ export class ViajesPublicadosComponent implements OnInit{
       })
   }
   
+  public deleteViajePublicado(id_viaje: number){
+    this.viajeService.deleteViaje(id_viaje).subscribe(()=>{
+      this.viajes_publicados = this.viajes_publicados.filter( pasajero => pasajero.id_viaje !=id_viaje)
+   });
+  }
+
 }
 
