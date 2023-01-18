53b7a57f4cf4065ee8ed53945618f975f21520f7
PabloSarsotoMa <p.sarsoto.m@gmail.com>
2023-01-18 11:13:51 +0100

    gg

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Router} from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
 import { Usuario } from 'src/app/models/usuario';
 import { UsuarioService } from 'src/shared/usuario.service';
 
@@ -11,21 +12,20 @@ import { UsuarioService } from 'src/shared/usuario.service';
 export class RegistroComponent {
   // public nuevoUsuario : string[];
 
-    constructor(public usuarioService: UsuarioService, private router : Router){    }
+    constructor(public usuarioService: UsuarioService, private router : Router, public toast:HotToastService){    }
     public registrar(nombre:string, apellidos:string, fechaDeNacimiento:string, email:string, password:string, password2:string){
       if(nombre=="" || apellidos=="" || fechaDeNacimiento == "" || email == "" || password== ""){
-        console.log("faltan campos por rellenar");
-        
-      }
-      else if(password != password2){
-       alert("Las contraseñas no coinciden");
-      }
-      else{
-         let nuevoUsuario = new Usuario(nombre,apellidos,fechaDeNacimiento, email, password,"","","","","","","",[],-1,-1,-1)
-         this.usuarioService.register(nuevoUsuario).subscribe((respuesta) => {
+        this.toast.warning("faltan campos por rellenar"); 
+      }else if(!email.includes("@") || !email.includes(".com")){
+        this.toast.warning("Introduce un email válido")
+      }else if(password != password2){
+        this.toast.warning("Las contraseñas no coinciden");
+      }else{
+        let nuevoUsuario = new Usuario(nombre,apellidos,fechaDeNacimiento, email, password,"","","","","","","",[],-1,-1,-1)
+          this.usuarioService.register(nuevoUsuario).subscribe((respuesta) => {
           this.router.navigateByUrl("login")
-         console.log(respuesta);
-       })
+          console.log(respuesta);
+        })
       }
     }
 }
